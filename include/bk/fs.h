2fa52672c6c77fd8c57a29f730048bf8c9fd5a79
Bach Le <bach@bullno1.com>
2018-03-20 01:05:35 +0800

    Move operation support check to bk/fs.h

--- include/bk/fs.h
+++ include/bk/fs.h
@@ -2,6 +2,7 @@
 #define BK_FS_H
 
 #include <stddef.h>
+#include <errno.h>
 #include "macro.h"
 
 typedef struct bk_fs_s bk_fs_t;
@@ -25,36 +26,48 @@ struct bk_fs_s
 BK_INLINE int
 bk_fopen(bk_fs_t* fs, const char* path, const char* mode, bk_file_t** file)
 {
+	if(fs->open == NULL) { return EOPNOTSUPP; }
+
 	return fs->open(fs, path, mode, file);
 }
 
 BK_INLINE void
 bk_fclose(bk_file_t* file)
 {
+	if(file->fs->close == NULL) { return; }
+
 	file->fs->close(file->fs, file);
 }
 
 BK_INLINE int
 bk_fread(bk_file_t* file, void* buf, size_t* len)
 {
+	if(file->fs->read == NULL) { return EOPNOTSUPP; }
+
 	return file->fs->read(file->fs, file, buf, len);
 }
 
 BK_INLINE int
 bk_fwrite(bk_file_t* file, const void* buf, size_t* len)
 {
+	if(file->fs->write == NULL) { return EOPNOTSUPP; }
+
 	return file->fs->write(file->fs, file, buf, len);
 }
 
 BK_INLINE int
 bk_ftell(bk_file_t* file, size_t* pos)
 {
+	if(file->fs->tell == NULL) { return EOPNOTSUPP; }
+
 	return file->fs->tell(file->fs, file, pos);
 }
 
 BK_INLINE int
 bk_fseek(bk_file_t* file, size_t pos, int origin)
 {
+	if(file->fs->seek == NULL) { return EOPNOTSUPP; }
+
 	return file->fs->seek(file->fs, file, pos, origin);
 }
 
--- src/stdstream.c
+++ src/stdstream.c
@@ -3,11 +3,6 @@
 #include <stdio.h>
 #include <errno.h>
 
-static int
-bk_stdstream_open(
-	bk_fs_t* fs, const char* path, const char* mode, bk_file_t** file
-);
-
 static void
 bk_stdstream_close(bk_fs_t* fs, bk_file_t* file);
 
@@ -17,19 +12,10 @@ bk_stdstream_read(bk_fs_t* fs, bk_file_t* file, void* buf, size_t* len);
 static int
 bk_stdstream_write(bk_fs_t* fs, bk_file_t* file, const void* buf, size_t* len);
 
-static int
-bk_stdstream_seek(bk_fs_t* fs, bk_file_t* file, size_t offset, int origin);
-
-static int
-bk_stdstream_tell(bk_fs_t* fs, bk_file_t* file, size_t* pos);
-
 static bk_fs_t bk_stdstream = {
-	.open = bk_stdstream_open,
 	.close = bk_stdstream_close,
 	.read = bk_stdstream_read,
 	.write = bk_stdstream_write,
-	.seek = bk_stdstream_seek,
-	.tell = bk_stdstream_tell
 };
 
 static bk_file_t bk_stdin_instance = { .fs = &bk_stdstream };
@@ -39,18 +25,6 @@ static bk_file_t bk_stderr_instance = { .fs = &bk_stdstream };
 bk_file_t* bk_stdin = &bk_stdin_instance;
 bk_file_t* bk_stdout = &bk_stdout_instance;
 bk_file_t* bk_stderr = &bk_stderr_instance;
-
-static int
-bk_stdstream_open(
-	bk_fs_t* fs, const char* path, const char* mode, bk_file_t** file
-)
-{
-	(void)fs;
-	(void)path;
-	(void)mode;
-	(void)file;
-	return EOPNOTSUPP;
-}
 
 static void
 bk_stdstream_close(bk_fs_t* fs, bk_file_t* file)
@@ -108,25 +82,4 @@ bk_stdstream_write(bk_fs_t* fs, bk_file_t* file, const void* buf, size_t* len)
 
 	*len = fwrite(buf, 1, *len, stdfile);
 	return ferror(stdfile) ? EIO : 0;
-}
-
-static int
-bk_stdstream_seek(bk_fs_t* fs, bk_file_t* file, size_t offset, int origin)
-{
-	(void)fs;
-	(void)file;
-	(void)offset;
-	(void)origin;
-
-	return EOPNOTSUPP;
-}
-
-static int
-bk_stdstream_tell(bk_fs_t* fs, bk_file_t* file, size_t* pos)
-{
-	(void)fs;
-	(void)file;
-	(void)pos;
-
-	return EOPNOTSUPP;
 }
