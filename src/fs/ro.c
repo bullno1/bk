09ceff314e9b237f6e42dede1543f0343923edbe
Bach Le <bach@bullno1.com>
2018-03-26 02:11:49 +0800

    Simplify ro fs

--- src/fs/ro.c
+++ src/fs/ro.c
@@ -18,49 +18,7 @@ bk_ro_fs_open(bk_fs_t* fs, const char* path, const char* mode, bk_file_t** file)
 
 	if(!bk_is_mode_ro(mode)) { return EROFS; }
 
-	int err = bk_fopen(self->backing_fs, path, mode, file);
-
-	(*file)->fs = fs;
-
-	return err;
-}
-
-static void
-bk_ro_fs_close(bk_fs_t* fs, bk_file_t* file)
-{
-	bk_ro_fs_t* self = BK_CONTAINER_OF(fs, bk_ro_fs_t, bk_fs);
-	self->backing_fs->close(self->backing_fs, file);
-}
-
-static int
-bk_ro_fs_read(bk_fs_t* fs, bk_file_t* file, void* buf, size_t* len)
-{
-	bk_ro_fs_t* self = BK_CONTAINER_OF(fs, bk_ro_fs_t, bk_fs);
-	return self->backing_fs->read(self->backing_fs, file, buf, len);
-}
-
-static int
-bk_ro_fs_write(bk_fs_t* fs, bk_file_t* file, const void* buf, size_t* len)
-{
-	(void)fs;
-	(void)file;
-	(void)buf;
-	(void)len;
-	return EROFS;
-}
-
-static int
-bk_ro_fs_seek(bk_fs_t* fs, bk_file_t* file, size_t offset, int origin)
-{
-	bk_ro_fs_t* self = BK_CONTAINER_OF(fs, bk_ro_fs_t, bk_fs);
-	return self->backing_fs->seek(self->backing_fs, file, offset, origin);
-}
-
-static int
-bk_ro_fs_tell(bk_fs_t* fs, bk_file_t* file, size_t* pos)
-{
-	bk_ro_fs_t* self = BK_CONTAINER_OF(fs, bk_ro_fs_t, bk_fs);
-	return self->backing_fs->tell(self->backing_fs, file, pos);
+	return bk_fopen(self->backing_fs, path, mode, file);
 }
 
 struct bk_fs_s*
@@ -68,14 +26,7 @@ bk_ro_fs_create(struct bk_allocator_s* allocator, struct bk_fs_s* backing_fs)
 {
 	bk_ro_fs_t* fs = BK_NEW(allocator, bk_ro_fs_t);
 	*fs = (bk_ro_fs_t){
-		.bk_fs = {
-			.open = &bk_ro_fs_open,
-			.close = &bk_ro_fs_close,
-			.read = &bk_ro_fs_read,
-			.write = &bk_ro_fs_write,
-			.tell = &bk_ro_fs_tell,
-			.seek = &bk_ro_fs_seek,
-		},
+		.bk_fs = { .open = &bk_ro_fs_open, },
 		.allocator = allocator,
 		.backing_fs = backing_fs
 	};
